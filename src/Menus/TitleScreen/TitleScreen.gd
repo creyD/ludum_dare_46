f3192c9b302de2879dc904e9462bc2391842eb9d
Conrad <grosserconrad@gmail.com>
2020-04-18 19:30:43 +0200

    Added sounds to titlescreen

--- src/Menus/TitleScreen/TitleScreen.gd
+++ src/Menus/TitleScreen/TitleScreen.gd
@@ -8,5 +8,6 @@ func _ready():
 	animation_player.play("__INIT__")
 
 func _on_Startup_startup_finished():
+	new_game_button.ignore_once = true # Pauls russian solution for ignoring the first sound click in the titlescreen
 	new_game_button.grab_focus()
 	animation_player.play("show_buttons")
--- src/Menus/TitleScreen/TitleScreenButton.gd
+++ src/Menus/TitleScreen/TitleScreenButton.gd
@@ -3,10 +3,27 @@ class_name TitleSceenButton
 
 export(String, FILE, "*.tscn,*.scn") var scene_to_load = ""
 export(bool) var quit = false
+onready var sound_focus = $Sounds/FocusChange
+onready var sound_select = $Sounds/OptionSelect
+var ignore_once = false
 
 func _pressed():
+	sound_select.play()
+	
+
+func _on_OptionSelect_finished():
 	if quit:
 		get_tree().quit()
 		return
 		
 	get_tree().change_scene(scene_to_load)
+
+
+func _on_TitleScreenButton_mouse_entered():
+	grab_focus()
+
+
+func _on_TitleScreenButton_focus_entered():
+	if not ignore_once:
+		sound_focus.play()
+	ignore_once = false
