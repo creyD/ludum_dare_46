723b765b3e80b9d2fff3f1de293b75031005f02d
Jan Schuffenhauer <explorerkiller@gmail.com>
2020-04-20 23:20:30 +0200

    Implemented most sounds (Missing Boss and Hero Laught))

--- src/Objects/Heart/Heart.gd
+++ src/Objects/Heart/Heart.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+var CHANNEL = 2
+#one sound will be chosen at random
+export var SoundLibary :PoolStringArray=[]
 
 func _on_Hurtbox_area_entered(area):
+	var sound = SoundLibary[rand_range(0,SoundLibary.size())]
+	SoundControler.pub_play_effect(sound,CHANNEL)
+	
 	queue_free()
--- src/Objects/Rubies/Rubies.gd
+++ src/Objects/Rubies/Rubies.gd
@@ -5,4 +5,5 @@ extends Node2D
 
 
 func _on_Hurtbox_area_entered(area):
+	SoundControler.pub_play_effect("res://Objects/Rubies/emerald3.wav",8)
 	queue_free()
