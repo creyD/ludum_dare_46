5f8e91bc13b56d653669dfca01c53b5a2ed03e33
Conrad <grosserconrad@gmail.com>
2020-04-20 23:50:29 +0200

    Temp card fix

--- src/Menus/DragNDrop/DragNDropUI.gd
+++ src/Menus/DragNDrop/DragNDropUI.gd
@@ -1,7 +1,7 @@
 extends Control
 
 var usedCards = [cards.BARREL,cards.BEAR,cards.EMPTY,cards.EMPTY,cards.EMPTY]
-onready var HBox = $HBoxContainer
+var cardPositions = [Vector2(0,150), Vector2(50,150), Vector2(100,150), Vector2(150,150), Vector2(200, 150)]
 
 enum cards {
 	BANANA,
@@ -16,33 +16,40 @@ enum cards {
 
 func _ready():
 	update_cards()
-	pass
 
 export var ObjectParent:NodePath
 
 func update_cards():
-	for child in HBox.get_children():
-		child.queue_free()
-	
-	for card in usedCards:
-		var newchild
-		match card:
+	var index = 0
+	while index < 5 and usedCards[index] != cards.EMPTY:
+		index += 1
+
+	for card in range(index + 1):
+		var newchild = []
+		match usedCards[card]:
 			cards.BANANA:
-				newchild=load("res://Objects/Banana/BananaCard.tscn").instance()
+				newchild.append(load("res://Objects/Banana/BananaCard.tscn").instance())
 			cards.BARRIERE:
-				newchild=(load("res://Objects/Barriere/BarrierCard.tscn").instance())
+				newchild.append(load("res://Objects/Barriere/BarrierCard.tscn").instance())
 			cards.BARREL:
-				newchild=(load("res://Objects/Barrel/BarrelCard.tscn").instance())
+				newchild.append(load("res://Objects/Barrel/BarrelCard.tscn").instance())
 			cards.TORCH:
-				newchild=(load("res://Objects/Torch/TorchCard.tscn").instance())
+				newchild.append(load("res://Objects/Torch/TorchCard.tscn").instance())
 			cards.BEAR:
-				newchild=(load("res://Objects/Traps/Bear/BearCard.tscn").instance())
+				newchild.append(load("res://Objects/Traps/Bear/BearCard.tscn").instance())
 			cards.FLAME:
-				newchild=(load("res://Objects/Traps/Flame/FlameCard.tscn").instance())
+				newchild.append(load("res://Objects/Traps/Flame/FlameCard.tscn").instance())
 			cards.SPIKE:
-				newchild=(load("res://Objects/Traps/Spike/SpikeCard.tscn").instance())
+				newchild.append(load("res://Objects/Traps/Spike/SpikeCard.tscn").instance())
 			#cards.SLIME:
 				#newchild=(load("res://Objects/Slime/SlimeCard.tscn").instance())
-		if(newchild!=null):
-			HBox.add_child(newchild)
+	for i in range(index + 1):
+		$CardsDisplay.add_child(shownCards[i])
+	shownCards[0].set_begin (cardPositions[0])
+	shownCards[1].set_begin (cardPositions[1])
+	shownCards[2].set_begin (cardPositions[2])
 
+	for i in range(index + 1):
+		shownCards[i].canNotPlace = true
+		shownCards[i].margin_bottom = shownCards[i].margin_top+32
+		shownCards[i].margin_right = shownCards[i].margin_left+32
